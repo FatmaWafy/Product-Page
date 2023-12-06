732ce656aea09de0ebc4a1ae29e494e48913957e
fatmaa1601 <fatmaa1601@gmail.com>
2023-12-06 21:29:19 +0200

    add main files

---
+++ script.js
@@ -0,0 +1,24 @@
+let IMG = document.getElementById("productimg");
+let BTN = document.getElementsByClassName("btn");
+
+BTNS[0].onclick = function () {
+  IMG.src = "./images/image1.png";
+  for (btn of BTNS) {
+    btn.classList.remove("active");
+  }
+  this.classList.add("active");
+};
+BTNS[1].onclick = function () {
+  IMG.src = "./images/image2.png";
+  for (btn of BTNS) {
+    btn.classList.remove("active");
+  }
+  this.classList.add("active");
+};
+BTNS[2].onclick = function () {
+  IMG.src = "./images/image3.png";
+  for (btn of BTNS) {
+    btn.classList.remove("active");
+  }
+  this.classList.add("active");
+};
